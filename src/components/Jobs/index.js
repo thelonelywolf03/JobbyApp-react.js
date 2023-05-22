a6bd30333822ad3007d61266a9fe61f52f5440ed
root <mithunshankar07@gmail.com>
2023-05-22 16:23:45 +0000

    Jobby App

---
+++ src/components/Jobs/index.js
@@ -0,0 +1,14 @@
+import Header from '../Header'
+import AllJobsSection from '../AllJobsSection'
+import './index.css'
+
+const Jobs = () => (
+  <>
+    <Header />
+    <div className="jobs-sections">
+      <AllJobsSection />
+    </div>
+  </>
+)
+
+export default Jobs
